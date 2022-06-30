fce348f7d496916bbd5066fc667dcb229df68e9d
ferralex <104802204+ferralex@users.noreply.github.com>
2022-06-30 11:41:24 +0900

    Update sql_practice.sql

--- sql_practice.sql
+++ sql_practice.sql
@@ -272,3 +272,143 @@ FROM admissions
 WHERE
   patient_id % 2 != 0
   AND attending_physician_id IN (1, 5, 19)
+
+--HARD LEVEL
+
+/*Show all of the patients grouped into weight groups.
+Show the total amount of patients in each weight group.
+Order the list by the weight group decending.
+
+For example, if they weight 100 to 109 they are placed in the 100 weight group, 110-119 = 110 weight group, etc.*/
+SELECT COUNT (weight) AS patients_in_group,
+CASE
+WHEN weight < 10 THEN 0
+WHEN weight >= 10 AND weight < 20 THEN 10
+WHEN weight >= 20 AND weight < 30 THEN 20
+WHEN weight >= 30 AND weight < 40 THEN 30
+WHEN weight >= 40 AND weight < 50 THEN 40
+WHEN weight >= 50 AND weight < 60 THEN 50
+WHEN weight >= 60 AND weight < 70 THEN 60
+WHEN weight >= 70 AND weight < 80 THEN 70
+WHEN weight >= 80 AND weight < 90 THEN 80
+WHEN weight >= 90 AND weight < 100 THEN 90
+WHEN weight >= 100 AND weight < 110 THEN 100
+WHEN weight >= 110 AND weight < 120 THEN 110
+WHEN weight >= 120 AND weight < 130 THEN 120
+WHEN weight >= 130 THEN 130 
+END weight_group
+FROM patients
+GROUP BY weight_group
+ORDER BY weight_group DESC
+
+/*Show patient_id, weight, height, isObese from the patients table.
+
+Display isObese as a boolean 0 or 1.
+
+Obese is defined as weight(kg)/(height(m)2) >= 30.
+
+weight is in units kg.
+
+height is in units cm.*/
+
+SELECT
+  patient_id,
+  weight,
+  height,
+  CASE --is obese case (=1)
+    WHEN weight / POWER((height / 100.0), 2) >= 30 THEN 1 --in NOT obese case (=0)
+    ELSE 0
+  END isObese
+FROM patients
+
+/*Show patient_id, first_name, last_name, and attending physician's specialty.
+Show only the patients who has a primary_diagnosis as 'Dementia' and the physician's first name is 'Lisa'
+
+Check patients, admissions, and physicians tables for required information.*/
+SELECT
+  pa.patient_id,
+  pa.first_name,
+  pa.last_name,
+  ph.specialty
+FROM patients AS pa
+  INNER JOIN admissions As ad ON pa.patient_id = ad.patient_id
+  INNER JOIN physicians as ph ON ad.attending_physician_id = ph.physician_id
+WHERE
+  ad.primary_diagnosis = 'Dementia'
+  AND ph.first_name = 'Lisa' 
+
+/*All patients who have gone through admissions, can see their medical documents on our site. Those patients are given a temporary password after their first admission. Show the patient_id and temp_password.
+
+The password must be the following, in order:
+1. patient_id
+2. the numerical length of patient's last_name
+3. year of patient's birth_date*/
+SELECT
+  pa.patient_id,
+  CONCAT(
+    pa.patient_id,
+    LEN(pa.last_name),
+    YEAR(birth_date)
+  ) AS temp_password
+FROM patients AS pa
+  INNER JOIN admissions As ad ON pa.patient_id = ad.patient_id
+GROUP BY pa.patient_id
+
+/*Each admission costs $50 for patients without insurance, and $10 for patients with insurance. All patients with an even patient_id have insurance.
+
+Give each patient a 'Yes' if they have insurance, and a 'No' if they don't have insurance. Add up the admission_total cost for each has_insurance group.*/
+SELECT
+  'Yes' AS has_insurance,
+  COUNT (*) * 10 cost_after_insurance
+FROM admissions
+WHERE patient_id % 2 = 0
+UNION
+SELECT
+  'No' AS has_insurance,
+  COUNT (*) * 50 cost_after_insurance
+FROM admissions
+WHERE patient_id % 2 != 0
+
+/*Show the provinces that has more patients identified as 'M' than 'F'. Must only show full province_name
+SELECT province_name*/
+FROM (
+    SELECT
+      province_name,
+      SUM(gender = 'M') AS n_male,
+      SUM(gender = 'F') AS n_female
+    FROM patients pa
+      JOIN provinces pr ON pa.province_id = pr.province_id
+    GROUP BY province_name
+    HAVING n_male > n_female
+  )
+
+/*We are looking for a specific patient. Pull all columns for the patient who matches the following criteria:
+- First_name contains an 'r' after the first two letters.
+- Identifies their gender as 'F'
+- Born in February, May, or December
+- Their weight would be between 60kg and 80kg
+- Their patient_id is an odd number
+- They are from the city 'Halifax'*/
+SELECT *
+FROM patients
+WHERE
+  first_name LIKE '__r%'
+  AND gender = 'F'
+  AND MONTH(birth_date) IN (2, 5, 12)
+  AND patient_id % 2.0 != 0
+  AND city = 'Halifax'
+
+/*Show the percent of patients that have 'M' as their gender. Round the answer to the nearest hundreth number and in percent form.*/
+SELECT
+  round(100 * avg(gender = 'M'), 2) || '%' AS percent_of_male_patients
+FROM patients;
+
+/*Show the patient_id and total_spent for patients who spent over 150 in combined medication_cost. Sort by most total_spent to least total_spent.*/
+SELECT
+  u.patient_id,
+  SUM(m.medication_cost) AS total_spent
+FROM medications AS m
+  INNER JOIN unit_dose_orders AS u ON m.medication_id = u.medication_id
+GROUP BY u.patient_id
+HAVING total_spent > 150
+ORDER BY total_spent DESC
